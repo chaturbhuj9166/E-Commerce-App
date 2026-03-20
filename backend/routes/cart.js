0391b7336e5483af19a37503ea2a3715e2bec4a5
Prem <joshichaturbhuj2@gmail.com>
2026-03-20 16:49:32 -0400

    last updated

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,31 +1,3 @@
-// import { Router } from "express";
-// import { addToCart, fetchCart, removeCartItem } from "../controllers/Cart.js";
-// import { checkForlogin } from "../middlewares/MiddlewaresAuth.js";
-
-// const cartRouter = Router();
-
-// // Add / update cart item
-// cartRouter.post(
-//   "/add",
-//   (req, res, next) => checkForlogin(req, res, next, "user"),
-//   addToCart
-// );
-
-// // Fetch cart items
-// cartRouter.get(
-//   "/",
-//   (req, res, next) => checkForlogin(req, res, next, "user"),
-//   fetchCart
-// );
-
-// // DELETE cart item
-// cartRouter.delete(
-//   "/remove/:id",
-//   (req, res, next) => checkForlogin(req, res, next, "user"),
-//   removeCartItem
-// );
-
-// export default cartRouter;
 
 
 
